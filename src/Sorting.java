9e58ccfa7990d79b8b15bfc822854999c0de4b07
lvnguyen14 <longvannguyen9@gmail.com>
2024-10-11 15:46:16 +0200

    Sorting ikke færdig

---
+++ src/Sorting.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class Sorting {
+
+    public void printProblemDescription(){
+        System.out.println("Write a method that has an integer parameter and an ArrayList. " +
+                "The program should ask the user how many words she would like to type. " +
+                "Use an arrayList to store the text strings from the keyboard and print " +
+                "them on the screen in alphabetical descending order. If the following " +
+                "strings are input: monkey mouse pineapple bike sunshine the output should be: " +
+                "bike monkey mouse pineapple sunshine");
+    }
+
+    private ArrayList<String> stringArrayList = new ArrayList<String>();
+
+    public void sortArrayList(){
+
+    }
+
+    public void addStr(String str){
+
+    }
+
+}
